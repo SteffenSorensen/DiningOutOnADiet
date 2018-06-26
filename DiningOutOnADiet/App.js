60139c58c09bfcd19b95543a5c00df90be52e6f9
Steffen Sørensen <sts@cubeio.com>
2018-06-26 12:01:26 +0200

    Initial commit after running react-native init and editing some text and running on an Android emulator.

---
+++ DiningOutOnADiet/App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Dining out on a diet!
+        </Text>
+        <Text style={styles.instructions}>
+          An opinionated guide for where to eat nearby when having special dietary needs.
+        </Text>
+        <Text style={styles.instructions}>
+          The knowledge base is contributed by the community to keep the information fresh and relevant. 
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
